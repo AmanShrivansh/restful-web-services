0f35f4fe8752471bf5753836736c44c188059c57
AmanShrivansh <amanshrivansh04@gmail.com>
2024-08-18 18:58:54 +0530

    Added exception handling

--- src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
@@ -1,12 +1,16 @@
 package com.in28minutes.rest.webservices.restful_web_services.user;
 
+import java.net.URI;
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -24,12 +28,23 @@ public class UserResource {
 	
 	@GetMapping(path="/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
-		return service.findOne(id);
+		User user = service.findOne(id);
+		if(user==null)
+			throw new UserNotFoundException("id:"+id);
+		return user;
 	}
 	
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
-		service.save(user);
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		
+		User savedUser = service.save(user);
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+						.path("/{id}")
+						.buildAndExpand(savedUser.getId())
+						.toUri();   
+		
+		return ResponseEntity.created(location).build();
 	}
 	
 }
