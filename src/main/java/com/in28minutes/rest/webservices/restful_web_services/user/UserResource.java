09e13695c1196957a2b02193b00e2bda640bfc9d
AmanShrivansh <amanshrivansh04@gmail.com>
2024-08-18 17:18:20 +0530

    Added  GET & POST

---
+++ src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
@@ -0,0 +1,35 @@
+package com.in28minutes.rest.webservices.restful_web_services.user;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+	//This is basically autowiring
+	private UserDaoService service = new UserDaoService();
+	
+	public UserResource(UserDaoService service) {
+		this.service = service;
+	}
+	
+	@GetMapping(path="/users")
+	public List<User> retrieveAllUsers() {
+		return service.findAll();
+	}
+	
+	@GetMapping(path="/users/{id}")
+	public User retrieveUser(@PathVariable int id) {
+		return service.findOne(id);
+	}
+	
+	@PostMapping("/users")
+	public void createUser(@RequestBody User user) {
+		service.save(user);
+	}
+	
+}
